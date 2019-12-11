3926634c8c438dfb87c1e5b0fb177799b4b83215
Casey Khuc <khuctrang1812@gmail.com>
2019-12-11 16:24:29 +0700

    modify config env.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h2>{process.env.REACT_APP_DOC_TITLE}</h2>
+        <p>{process.env.REACT_APP_API_URL}</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
